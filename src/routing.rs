98faebf808683d0f1667d17a814562b91dc395ea
hizani <>
2024-12-01 17:53:16 +0300

    update closest_nodes to check only closest buckets

--- src/routing.rs
+++ src/routing.rs
@@ -58,8 +58,7 @@ impl RoutingTable {
     /// Update the appropriate bucket with the new node's info
     pub fn update(&self, node_info: NodeInfo) {
         let bucket_index = self.lookup_bucket_index(node_info.id);
-        let table = &self.buckets;
-        let mut bucket = table[bucket_index].lock().unwrap();
+        let mut bucket = self.buckets[bucket_index].lock().unwrap();
         let node_index = bucket.iter().position(|x| x.id == node_info.id);
         match node_index {
             Some(i) => {
@@ -78,31 +77,58 @@ impl RoutingTable {
     }
 
     /// Lookup the nodes closest to item in this table
-    ///
-    /// NOTE: This method is a really stupid, linear time search. I can't find
-    /// info on how to use the buckets effectively to solve this.
     pub fn closest_nodes(&self, item: Key, count: usize) -> Vec<NodeAndDistance> {
         if count == 0 {
             return Vec::new();
         }
-        let mut ret = Vec::with_capacity(count);
-        for bucket in self.buckets.iter() {
-            for node_info in bucket.lock().unwrap().iter() {
-                ret.push(NodeAndDistance(
-                    node_info.clone(),
-                    node_info.id.distance(item),
-                ));
+
+        let closest_bucket_index = self.lookup_bucket_index(item);
+        let closest_bucket = self.buckets.index(closest_bucket_index);
+
+        let mut closest_nodes: Vec<NodeAndDistance> = Vec::with_capacity(count);
+        closest_nodes.extend(
+            closest_bucket
+                .lock()
+                .unwrap()
+                .iter()
+                .map(|node_info| NodeAndDistance(node_info.clone(), node_info.id.distance(item))),
+        );
+
+        let mut delta = 0;
+
+        while closest_nodes.len() < count {
+            delta += 1;
+
+            let (left_index, is_left_overflow) = closest_bucket_index.overflowing_sub(delta);
+            let right_index = closest_bucket_index + delta;
+
+            let check_buckets = if is_left_overflow {
+                (None, self.buckets.get(right_index))
+            } else {
+                (self.buckets.get(left_index), self.buckets.get(right_index))
+            };
+
+            if let Some(left_bucket) = check_buckets.0 {
+                closest_nodes.extend(left_bucket.lock().unwrap().iter().map(|node_info| {
+                    NodeAndDistance(node_info.clone(), node_info.id.distance(item))
+                }));
+            }
+
+            if let Some(right_bucket) = check_buckets.1 {
+                closest_nodes.extend(right_bucket.lock().unwrap().iter().map(|node_info| {
+                    NodeAndDistance(node_info.clone(), node_info.id.distance(item))
+                }));
             }
         }
-        ret.sort_by(|a, b| a.1.cmp(&b.1));
-        ret.truncate(count);
-        ret
+
+        closest_nodes.sort_by(|a, b| a.1.cmp(&b.1));
+        closest_nodes.truncate(count);
+        closest_nodes
     }
 
     pub fn remove(&self, node_info: &NodeInfo) {
         let bucket_index = self.lookup_bucket_index(node_info.id);
-        let table = &self.buckets;
-        let mut bucket = table.index(bucket_index).lock().unwrap();
+        let mut bucket = self.buckets.index(bucket_index).lock().unwrap();
         if let Some(item_index) = bucket.iter().position(|x| x == node_info) {
             bucket.remove(item_index);
         } else {
